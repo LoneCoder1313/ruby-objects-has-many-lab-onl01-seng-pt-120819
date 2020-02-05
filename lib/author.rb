28151c7b15715c0844b0baeac8156f511e8e1538
Ricky dildy <rdildy92@gmail.com>
2020-02-05 22:48:31 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,32 @@
+class Author 
+  attr_accessor :name
+  @@post_count = 0 
+  
+  def initialize(name)
+    @name = name 
+    @post = []
+  end 
+  
+  def post 
+    @post
+  end 
+  
+  def add_post(post)
+    @post << post 
+    post.author = self 
+    @@post_count += 1 
+  end 
+  
+  def add_post_by_title(post, title)
+  
+   post = Post.new(post_title)
+    @posts << post
+    post.author = self
+    @@post_count += 1
+  end
+
+  def self.post_count
+    @@post_count
+  end
+
+end 
