2c266ccd7b24a670b7b9a7bf7e135a31fd093b03
Ricky dildy <rdildy92@gmail.com>
2020-02-05 22:49:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,16 +1,8 @@
 class Song
   attr_accessor :artist, :name
 
-  def initialize(name)
-    @name = name
-  end
-
-  def artist_name
-    if artist
-      self.artist.name
-    else
-      nil
-    end
-  end
-
-end 
+  @@all = []
+  
+def initialize(name)
+  @name = name 
+  @@all 
